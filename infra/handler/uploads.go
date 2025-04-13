5932d462bc9563cb7b9e48fbfe3204d3ecab40fc
Lukas Wolfsberger <1245519+wolfsblu@users.noreply.github.com>
2025-04-13 17:00:58 +0200

    Configure tusd

--- infra/handler/uploads.go
+++ infra/handler/uploads.go
@@ -1,15 +1,28 @@
 package handler
 
 import (
+	"github.com/tus/tusd/v2/pkg/filelocker"
+	"github.com/tus/tusd/v2/pkg/filestore"
 	tusd "github.com/tus/tusd/v2/pkg/handler"
+	"github.com/wolfsblu/go-chef/infra/env"
 )
 
 func NewUploadHandler() (*tusd.Handler, error) {
+	store := filestore.New(env.MustGet("UPLOAD_FOLDER"))
+	locker := filelocker.New(env.MustGet("UPLOAD_FOLDER"))
+
+	composer := tusd.NewStoreComposer()
+	store.UseIn(composer)
+	locker.UseIn(composer)
+
 	handler, err := tusd.NewHandler(tusd.Config{
-		BasePath: "/files/",
+		BasePath:              "/files/",
+		StoreComposer:         composer,
+		NotifyCompleteUploads: true,
 	})
 	if err != nil {
 		return nil, err
 	}
+
 	return handler, nil
 }
