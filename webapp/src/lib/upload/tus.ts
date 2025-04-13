ad75060a28262cd1730c1e7b4927367194a9374f
Lukas Wolfsberger <1245519+wolfsblu@users.noreply.github.com>
2025-04-13 21:41:06 +0200

    Add sample gallery

--- webapp/src/lib/upload/tus.ts
+++ webapp/src/lib/upload/tus.ts
@@ -1,24 +1,44 @@
 import { Upload } from 'tus-js-client'
 import { PUBLIC_API_URL } from "$env/static/public";
 
-export const uploadFile = async (file: File) => {
-    const upload = new Upload(file, {
-        endpoint: PUBLIC_API_URL + '/uploads/',
-        retryDelays: [0, 3000, 5000, 10000, 20000],
-        metadata: {
-            filename: file.name,
-            filetype: file.type,
-        },
-        onBeforeRequest: (req) => {
-            const xhr = req.getUnderlyingObject()
-            xhr.withCredentials = true
-        },
+interface UploadResult {
+    file: File
+    url: string | null
+}
+
+export const uploadFile = async (file: File): Promise<UploadResult> => {
+    return new Promise(async (resolve, reject) => {
+        const upload = new Upload(file, {
+            endpoint: PUBLIC_API_URL + '/uploads/',
+            retryDelays: [0, 3000, 5000, 10000, 20000],
+            metadata: {
+                filename: file.name,
+                filetype: file.type,
+            },
+            onBeforeRequest: (req) => {
+                const xhr = req.getUnderlyingObject()
+                xhr.withCredentials = true
+            },
+            onSuccess: () => {
+                resolve({
+                    file: upload.file,
+                    url: upload.url
+                })
+            },
+            onError: (err) => {
+                reject(err)
+            }
+        })
+
+        try {
+            const previousUploads = await upload.findPreviousUploads()
+            if (previousUploads.length) {
+                upload.resumeFromPreviousUpload(previousUploads[0])
+            }
+
+            upload.start()
+        } catch (e) {
+            reject(e)
+        }
     })
-
-    const previousUploads = await upload.findPreviousUploads()
-    if (previousUploads.length) {
-        upload.resumeFromPreviousUpload(previousUploads[0])
-    }
-
-    upload.start()
 }
--- webapp/src/routes/(authenticated)/recipes/+page.svelte
+++ webapp/src/routes/(authenticated)/recipes/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
     import { uploadFile } from '$lib/upload/tus'
-    import { Fileupload, SpeedDial, SpeedDialButton } from "flowbite-svelte";
+    import {type ImgType, Fileupload, Gallery, SpeedDial, SpeedDialButton} from "flowbite-svelte";
     import {
         ShoppingBagSolid,
         ReceiptSolid,
         RulerCombinedOutline
     } from "flowbite-svelte-icons";
     import type { ChangeEventHandler } from "svelte/elements";
+
+    const images = $state<ImgType[]>([]);
 
     const onFileChange: ChangeEventHandler<HTMLInputElement> = async (e) => {
         const files = (e.target as HTMLInputElement)?.files
@@ -18,13 +20,27 @@
             const file = files[i]
             uploadPromises.push(uploadFile(file))
         }
-        await Promise.allSettled(uploadPromises)
+
+        const uploads = await Promise.allSettled(uploadPromises)
+        for (let i = 0; i < uploads.length; ++i) {
+            const upload = uploads[i]
+            if (upload.status === "fulfilled" && upload.value.url) {
+                images.push({
+                    alt: "Image",
+                    src: upload.value.url,
+                })
+            }
+        }
     }
     
     const iconClass = "w-6 h-6"
 </script>
 
 <Fileupload accept="image/*" multiple onchange={onFileChange} />
+
+<Gallery items={images} class="gap-4 grid-cols-2 md:grid-cols-4">
+
+</Gallery>
 
 <SpeedDial>
     <SpeedDialButton name="Measurement">
