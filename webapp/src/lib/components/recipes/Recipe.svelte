07853ad75922029925d638349e298628033fc2de
Lukas Wolfsberger <1245519+wolfsblu@users.noreply.github.com>
2025-04-14 22:41:03 +0200

    Move recipe to separate component

---
+++ webapp/src/lib/components/recipes/Recipe.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+    import placeholder from '$lib/components/recipes/fruits.jpg'
+    import {
+        Badge,
+        Button,
+        Card,
+        Dropdown,
+        DropdownDivider,
+        DropdownItem,
+        Listgroup,
+        ListgroupItem
+    } from "flowbite-svelte";
+    import {DotsVerticalOutline} from "flowbite-svelte-icons";
+
+    const listItemClass = "px-0 flex justify-between"
+
+    interface Props {
+        title: string
+        ingredients: number
+        servings: number
+        minutes: number
+    }
+
+    let {
+        title,
+        ingredients,
+        servings,
+        minutes,
+    }: Props = $props()
+</script>
+
+<Card class="card" img={placeholder} imgClass="max-h-42 object-cover" padding="sm" size="none">
+    <div class="flex flex-col flex-grow justify-between">
+        <section class="mb-2 flex gap-1 justify-between items-top">
+            <h1 class="text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+                {title}
+            </h1>
+            <Button class="h-7 w-7 p-2" outline pill>
+                <DotsVerticalOutline class="w-6 h-6"/>
+            </Button>
+            <Dropdown>
+                <DropdownItem>Edit</DropdownItem>
+                <DropdownDivider/>
+                <DropdownItem>Delete</DropdownItem>
+            </Dropdown>
+        </section>
+        <Listgroup class="border-0 dark:bg-transparent!">
+            <ListgroupItem normalClass={listItemClass}>
+                <span>Time</span>
+                <span>{minutes} min</span>
+            </ListgroupItem>
+            <ListgroupItem normalClass={listItemClass}>
+                <span>Servings</span>
+                <Badge>{servings}</Badge>
+            </ListgroupItem>
+            <ListgroupItem normalClass={listItemClass}>
+                <span>Ingredients</span>
+                <Badge>{ingredients}</Badge>
+            </ListgroupItem>
+        </Listgroup>
+    </div>
+</Card>
+
+<style>
+    :global(.card > div) {
+        display: flex;
+        flex-grow: 1;
+    }
+</style>
--- webapp/src/routes/(authenticated)/recipes/+page.svelte
+++ webapp/src/routes/(authenticated)/recipes/+page.svelte
@@ -3,25 +3,17 @@
         SpeedDial,
         SpeedDialButton,
         Search,
-        Button,
-        Card,
-        Listgroup,
-        ListgroupItem,
-        Badge,
-        Dropdown,
-        DropdownItem,
-        DropdownDivider
+        Button
     } from "flowbite-svelte";
     import {
         ShoppingBagSolid,
         ReceiptSolid,
         RulerCombinedOutline,
-        SearchOutline,
-        DotsVerticalOutline
+        SearchOutline
     } from "flowbite-svelte-icons";
+    import Recipe from "$lib/components/recipes/Recipe.svelte";
 
     const iconClass = "w-6 h-6"
-    const listItemClass = "px-0 flex justify-between"
 </script>
 
 <form class="flex gap-2 mb-4">
@@ -32,47 +24,13 @@
     </Button>
 </form>
 
-{#snippet DummyCard(index: number)}
-    <Card img="/recipes/fruits.jpg" imgClass="max-h-42 object-cover" padding="sm" size="none" class="card">
-        <div class="flex flex-col flex-grow justify-between">
-            <section class="mb-2 flex gap-1 justify-between items-top">
-                <h1 class="text-lg font-bold tracking-tight text-gray-900 dark:text-white">
-                    {#if index === 2}
-                        My super duper super duper super duper super tasty recipe
-                    {:else}
-                        My super duper tasty recipe
-                    {/if}
-                </h1>
-                <Button class="h-7 w-7 p-2" outline pill>
-                    <DotsVerticalOutline class="w-6 h-6" />
-                </Button>
-                <Dropdown>
-                    <DropdownItem>Edit</DropdownItem>
-                    <DropdownDivider />
-                    <DropdownItem>Delete</DropdownItem>
-                </Dropdown>
-            </section>
-            <Listgroup class="border-0 dark:bg-transparent!">
-                <ListgroupItem normalClass={listItemClass}>
-                    <span>Time</span>
-                    <span>45 min</span>
-                </ListgroupItem>
-                <ListgroupItem normalClass={listItemClass}>
-                    <span>Servings</span>
-                    <Badge>4</Badge>
-                </ListgroupItem>
-                <ListgroupItem normalClass={listItemClass}>
-                    <span>Ingredients</span>
-                    <Badge>12</Badge>
-                </ListgroupItem>
-            </Listgroup>
-        </div>
-    </Card>
+{#snippet DummyRecipe(title: string, servings: number, ingredients: number, minutes: number)}
+    <Recipe title={title} servings={servings} ingredients={ingredients} minutes={minutes} />
 {/snippet}
 
 <section class="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
     {#each {length: 4} as _, i}
-        {@render DummyCard(i)}
+        {@render DummyRecipe("My super super tasty recipe", 4, 12, 45)}
     {/each}
 </section>
 
@@ -87,10 +45,3 @@
         <ReceiptSolid class={iconClass} />
     </SpeedDialButton>
 </SpeedDial>
-
-<style>
-    :global(.card > div) {
-        display: flex;
-        flex-grow: 1;
-    }
-</style>
