39dd58c87c469b983f595224869b4382f0c99c5a
Lukas Wolfsberger <1245519+wolfsblu@users.noreply.github.com>
2025-04-14 22:26:28 +0200

    Prepare recipe layout

--- webapp/src/routes/(authenticated)/recipes/+page.svelte
+++ webapp/src/routes/(authenticated)/recipes/+page.svelte
@@ -1,37 +1,80 @@
 <script lang="ts">
-    import { uploadFile } from '$lib/upload/tus'
-    import {type ImgType, Fileupload, Gallery, SpeedDial, SpeedDialButton} from "flowbite-svelte";
+    import {
+        SpeedDial,
+        SpeedDialButton,
+        Search,
+        Button,
+        Card,
+        Listgroup,
+        ListgroupItem,
+        Badge,
+        Dropdown,
+        DropdownItem,
+        DropdownDivider
+    } from "flowbite-svelte";
     import {
         ShoppingBagSolid,
         ReceiptSolid,
-        RulerCombinedOutline
+        RulerCombinedOutline,
+        SearchOutline,
+        DotsVerticalOutline
     } from "flowbite-svelte-icons";
-    import type { ChangeEventHandler } from "svelte/elements";
 
-    const images = $state<ImgType[]>([]);
-
-    const onFileChange: ChangeEventHandler<HTMLInputElement> = async (e) => {
-        const files = (e.target as HTMLInputElement)?.files
-        if (files == null) {
-            return
-        }
-        for (let i = 0; i < files.length; ++i) {
-            const file = files[i]
-            const upload = await uploadFile(file)
-            images.push({
-                alt: "Image",
-                src: upload.url ?? "",
-            })
-        }
-    }
-    
     const iconClass = "w-6 h-6"
+    const listItemClass = "px-0 flex justify-between"
 </script>
 
-<Fileupload accept="image/*" multiple onchange={onFileChange} />
+<form class="flex gap-2 mb-4">
+    <Search wrapperClass="relative w-full" class="h-full" placeholder="Find a recipe" />
+    <Button class="cursor-pointer">
+        <SearchOutline class="w-6 h-6 me-2 -ms-1" />
+        Search
+    </Button>
+</form>
 
-<Gallery items={images} class="gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-8">
-</Gallery>
+{#snippet DummyCard(index: number)}
+    <Card img="/recipes/fruits.jpg" imgClass="max-h-42 object-cover" padding="sm" size="none" class="card">
+        <div class="flex flex-col flex-grow justify-between">
+            <section class="mb-2 flex gap-1 justify-between items-top">
+                <h1 class="text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+                    {#if index === 2}
+                        My super duper super duper super duper super tasty recipe
+                    {:else}
+                        My super duper tasty recipe
+                    {/if}
+                </h1>
+                <Button class="h-7 w-7 p-2" outline pill>
+                    <DotsVerticalOutline class="w-6 h-6" />
+                </Button>
+                <Dropdown>
+                    <DropdownItem>Edit</DropdownItem>
+                    <DropdownDivider />
+                    <DropdownItem>Delete</DropdownItem>
+                </Dropdown>
+            </section>
+            <Listgroup class="border-0 dark:bg-transparent!">
+                <ListgroupItem normalClass={listItemClass}>
+                    <span>Time</span>
+                    <span>45 min</span>
+                </ListgroupItem>
+                <ListgroupItem normalClass={listItemClass}>
+                    <span>Servings</span>
+                    <Badge>4</Badge>
+                </ListgroupItem>
+                <ListgroupItem normalClass={listItemClass}>
+                    <span>Ingredients</span>
+                    <Badge>12</Badge>
+                </ListgroupItem>
+            </Listgroup>
+        </div>
+    </Card>
+{/snippet}
+
+<section class="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    {#each {length: 4} as _, i}
+        {@render DummyCard(i)}
+    {/each}
+</section>
 
 <SpeedDial>
     <SpeedDialButton name="Measurement">
@@ -44,3 +87,10 @@
         <ReceiptSolid class={iconClass} />
     </SpeedDialButton>
 </SpeedDial>
+
+<style>
+    :global(.card > div) {
+        display: flex;
+        flex-grow: 1;
+    }
+</style>
