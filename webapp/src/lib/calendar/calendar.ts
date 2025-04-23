6e543cf0174b9c729b939e718f58b303bc40080e
Lukas Wolfsberger <1245519+wolfsblu@users.noreply.github.com>
2025-04-23 21:19:13 +0200

    Temp

---
+++ webapp/src/lib/calendar/calendar.ts
@@ -0,0 +1,29 @@
+export const getDaysInMonth = (date: Date) => {
+    const year = date.getFullYear();
+    const month = date.getMonth(); // 0 = January
+
+    const firstDayOfMonth = new Date(year, month, 1);
+    // Weekday index (Monday = 0, Sunday = 6)
+    const startDayIndex = (firstDayOfMonth.getDay() + 6) % 7;
+
+    const totalDays = 42;
+    const dates: Date[] = [];
+
+    const startDate = new Date(year, month, 1 - startDayIndex);
+
+    for (let i = 0; i < totalDays; i++) {
+        const date = new Date(startDate);
+        date.setDate(startDate.getDate() + i);
+        dates.push(date);
+    }
+
+    return dates;
+}
+
+export const isToday = (date: Date) => {
+    return date.toDateString() === new Date().toDateString();
+}
+
+export const inCurrentMonth = (date: Date) => {
+    return date.getMonth() === new Date().getMonth();
+}
--- webapp/src/routes/(authenticated)/recipes/plan/+page.svelte
+++ webapp/src/routes/(authenticated)/recipes/plan/+page.svelte
@@ -1,14 +1,37 @@
 <script lang="ts">
-    import { Alert } from "flowbite-svelte";
-    import { InfoCircleSolid } from "flowbite-svelte-icons";
+    import {Alert, Card, Group, GroupItem, Timeline, TimelineItem} from "flowbite-svelte";
+    import {InfoCircleSolid} from "flowbite-svelte-icons";
+    import {getDaysInMonth, inCurrentMonth, isToday} from "$lib/calendar/calendar";
 
     // TODO: Use https://wicky.nillia.ms/cally/components/calendar-range/ for selecting the date range
     // TODO: Display as calendar next to schedule https://tailwindcss.com/plus/ui-blocks/application-ui/data-display/calendars#component-5bb74a1ac1c8cf96e7b3d1ea2bf4570b
+
+    const now = new Date();
+    const days = getDaysInMonth(now);
+
+    let groupTimelines = [
+        {
+            title: '<span class="font-medium text-gray-900 dark:text-white">Jese Leos</span> likes <span class="font-medium text-gray-900 dark:text-white">Bonnie Green\'s</span> post in <span class="font-medium text-gray-900 dark:text-white"> How to start with Flowbite library</span>',
+            src: '/images/profile-picture-1.webp',
+            alt: 'alt here',
+            href: '/',
+            isPrivate: true,
+            comment: '"I wanted to share a webinar zeroheight."'
+        },
+        {
+            title: '<span class="font-medium text-gray-900 dark:text-white">Bonnie Green</span> react to <span class="font-medium text-gray-900 dark:text-white">Thomas Lean\'s</span> comment',
+            src: '/images/profile-picture-2.webp',
+            alt: 'alt here',
+            href: '/',
+            isPrivate: true,
+            comment: '"I wanted to share a webinar zeroheight."'
+        }
+    ]
 </script>
 
-<Alert border color="blue" class="items-start!">
+<Alert border class="items-start! mb-3" color="blue">
     <span slot="icon">
-        <InfoCircleSolid class="w-5 h-5" />
+        <InfoCircleSolid class="w-5 h-5"/>
         <span class="sr-only">Info</span>
     </span>
     <p class="font-medium">
@@ -17,3 +40,35 @@
     </p>
 </Alert>
 
+<div class="grid grid-cols-3 gap-3">
+    <Card size="none">
+        <div class="grid grid-cols-7 overflow-hidden rounded-md border border-gray-200 dark:border-gray-600">
+            {#each days as date, i}
+                <div class="flex justify-center p-1 border-t border-l border-gray-200 dark:border-gray-600 font-semibold
+                {i % 7 === 0 ? 'border-l-0' : ''}
+                {i < 7 ? 'border-t-0' : ''}
+                {inCurrentMonth(date) ? 'text-gray-700 dark:text-gray-100' : 'bg-gray-100 dark:bg-gray-700'}"
+                >
+                    <time class="flex items-center justify-center w-10 h-10
+                    {isToday(date) ? ' bg-primary-300 rounded-full text-primary-600 dark:text-primary-400 dark:bg-primary-600' : ''}"
+                    >
+                        {date.getDate()}
+                    </time>
+                </div>
+            {/each}
+        </div>
+    </Card>
+    <Card size="none" class="col-span-2">
+        <Timeline>
+            <TimelineItem title="Application UI code in Tailwind CSS" date="February 2022">
+                <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
+            </TimelineItem>
+            <TimelineItem title="Application UI code in Tailwind CSS" date="March 2022">
+                <p class="text-base font-normal text-gray-500 dark:text-gray-400">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
+            </TimelineItem>
+            <TimelineItem title="Application UI code in Tailwind CSS" date="April 2022">
+                <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements built on top of Tailwind CSS.</p>
+            </TimelineItem>
+        </Timeline>
+    </Card>
+</div>
