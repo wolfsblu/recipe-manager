3590f0a7f918f85d55c77ecd86c8f4e613a44e07
Lukas Wolfsberger <1245519+wolfsblu@users.noreply.github.com>
2025-05-01 15:16:26 +0200

    Implement final draft of meal planner

--- webapp/src/lib/calendar/calendar.ts
+++ webapp/src/lib/calendar/calendar.ts
@@ -22,17 +22,24 @@ export const getDaysInMonth = (date: Date) => {
     return dates;
 }
 
-export const isToday = (date: Date) => {
-    return date.toDateString() === new Date().toDateString();
+export const isToday = (date: DateTime) => {
+    return date.hasSame(DateTime.local(), 'day')
 }
 
 export const inCurrentMonth = (date: Date) => {
     return date.getMonth() === new Date().getMonth();
 }
 
-export const getWeekdays = (locale: string = Settings.defaultLocale) => {
-    const weekdays = Info.weekdaysFormat('short', { locale });
+export const getWeekdays = (startDate: DateTime, locale = Settings.defaultLocale) => {
     const firstDay = Info.getStartOfWeek({ locale }); // returns 1 for most of Europe, 0 for US
-    // Reorder weekdays starting from the locale's first day of the week
-    return [...weekdays.slice(firstDay), ...weekdays.slice(0, firstDay)];
+    const weekday = startDate.weekday % 7
+    const startOfWeek = startDate.minus({ days: (7 + weekday - firstDay) % 7 })
+
+    return Array.from({ length: 7 }, (_, i) => {
+        const day = startOfWeek.plus({ days: i })
+        return {
+            name: day.toLocaleString({ weekday: 'short' }),
+            date: day,
+        };
+    });
 }
--- webapp/src/routes/(authenticated)/recipes/plan/+page.svelte
+++ webapp/src/routes/(authenticated)/recipes/plan/+page.svelte
@@ -1,34 +1,15 @@
 <script lang="ts">
-    import {Badge, Button, ButtonGroup, Card, Timeline, TimelineItem} from "flowbite-svelte";
-    import {AngleLeftOutline, AngleRightOutline, EditOutline, TrashBinOutline} from "flowbite-svelte-icons";
-    import {getDaysInMonth, getWeekdays, isToday} from "$lib/calendar/calendar";
-    import Recipe from "$lib/components/recipes/Recipe.svelte";
+    import {Button, Card, Timeline, TimelineItem} from "flowbite-svelte";
+    import {AngleLeftOutline, AngleRightOutline} from "flowbite-svelte-icons";
+    import {getWeekdays, isToday} from "$lib/calendar/calendar";
     import PlannedRecipe from "$lib/components/recipes/PlannedRecipe.svelte";
+    import {DateTime, Settings} from "luxon";
 
-    const now = new Date();
-    const days = getDaysInMonth(now);
-    const weekdays = getWeekdays()
+    const weekdays = getWeekdays(DateTime.now())
 </script>
 
-{#snippet Col()}
-    <div class="grid grid-cols-7 gap-3">
-        {#each {length: 7} as _, i}
-            {#if i !== 2}
-                <PlannedRecipe title="My super duper recipe" />
-            {:else}
-                <span></span>
-            {/if}
-        {/each}
-    </div>
-{/snippet}
-
-<Card size="none">
-    <div>
-        {#each weekdays as weekday}
-            <span>{weekday}</span>
-        {/each}
-    </div>
-    <div class="flex flex-col sm:flex-row justify-between items-center pb-4 gap-3">
+<Card size="none" padding="none">
+    <div class="flex flex-col sm:flex-row justify-between items-center pb-4 gap-3 p-6">
         <h2 class="text-2xl font-semibold">January 2022</h2>
         <div class="flex space-x-2">
             <Button color="alternative">
@@ -42,15 +23,20 @@
             </Button>
         </div>
     </div>
-    <Timeline>
-        <TimelineItem date="Breakfast">
-            {@render Col()}
-        </TimelineItem>
-        <TimelineItem date="Lunch">
-            {@render Col()}
-        </TimelineItem>
-        <TimelineItem date="Dinner">
-            {@render Col()}
-        </TimelineItem>
-    </Timeline>
+
+    <div class="grid grid-cols-7 border-y">
+        {#each weekdays as weekday}
+            <div class="text-center py-3 dark:text-white {isToday(weekday.date) ? 'bg-primary-200 dark:bg-primary-900' : ''}">
+                <p class="font-thin mb-1">{weekday.name.toUpperCase()}</p>
+                <p class="font-bold">{weekday.date.toLocaleString({day: "numeric"})}</p>
+            </div>
+        {/each}
+    </div>
+    <div class="grid grid-cols-7">
+        {#each weekdays as weekday}
+            <div class="p-3 pb-6 {isToday(weekday.date) ? 'bg-primary-200 dark:bg-primary-900' : ''}">
+                <PlannedRecipe title="My super duper recipe" />
+            </div>
+        {/each}
+    </div>
 </Card>
