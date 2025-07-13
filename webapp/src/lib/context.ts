2b0afb8d8fef281bbc0a863a7f599c32ab461fae
Lukas Wolfsberger <1245519+wolfsblu@users.noreply.github.com>
2025-07-13 18:47:50 +0200

    Small improvement

--- webapp/src/lib/context.ts
+++ webapp/src/lib/context.ts
@@ -1,4 +1,6 @@
 import { Context } from 'runed';
 import type { UseBoolean } from './hooks/use-boolean.svelte';
 
-export const commandContext = new Context<UseBoolean>('command-menu-context');
+const MenuKey = 'command-menu-context'
+
+export const commandContext = new Context<UseBoolean>(MenuKey);
