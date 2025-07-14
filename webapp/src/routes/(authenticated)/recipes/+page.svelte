51057ff273091d54f92af530aa972f71b7ed8888
Lukas Wolfsberger <1245519+wolfsblu@users.noreply.github.com>
2025-07-14 17:18:11 +0200

    Prepare recipe card

--- webapp/src/routes/(authenticated)/recipes/+page.svelte
+++ webapp/src/routes/(authenticated)/recipes/+page.svelte
@@ -1,4 +1,38 @@
 <script lang="ts">
+    import * as Card from "$lib/components/ui/card/index.js";
+    import * as Pagination from "$lib/components/ui/pagination/index.js";
+    import RecipeCard from "$lib/components/recipes/RecipeCard.svelte";
 </script>
 
-<p>Recipes</p>
+<div class="flex flex-col h-full justify-between p-5">
+<div class="grid grid-cols-5 gap-5">
+    {#each Array(10) as _, i}
+        <RecipeCard />
+    {/each}
+</div>
+<Pagination.Root count={100} perPage={10}>
+    {#snippet children({ pages, currentPage })}
+        <Pagination.Content>
+            <Pagination.Item>
+                <Pagination.PrevButton />
+            </Pagination.Item>
+            {#each pages as page (page.key)}
+                {#if page.type === "ellipsis"}
+                    <Pagination.Item>
+                        <Pagination.Ellipsis />
+                    </Pagination.Item>
+                {:else}
+                    <Pagination.Item>
+                        <Pagination.Link {page} isActive={currentPage === page.value}>
+                            {page.value}
+                        </Pagination.Link>
+                    </Pagination.Item>
+                {/if}
+            {/each}
+            <Pagination.Item>
+                <Pagination.NextButton />
+            </Pagination.Item>
+        </Pagination.Content>
+    {/snippet}
+</Pagination.Root>
+</div>
