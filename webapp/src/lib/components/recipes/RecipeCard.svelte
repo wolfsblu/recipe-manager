ec399eed7ea03ca62a5e4eab93550ea51cb3702f
Lukas Wolfsberger <1245519+wolfsblu@users.noreply.github.com>
2025-07-15 20:56:03 +0200

    Implement recipe tags

--- webapp/src/lib/components/recipes/RecipeCard.svelte
+++ webapp/src/lib/components/recipes/RecipeCard.svelte
@@ -1,32 +1,44 @@
 <script lang="ts">
     import fruits from './fruits.jpg'
+    import ClockIcon from '@lucide/svelte/icons/clock'
     import { Badge } from "$lib/components/ui/badge/index.js";
+    import { ScrollArea } from "$lib/components/ui/scroll-area/index.js";
     import { Separator } from "$lib/components/ui/separator/index.js";
 </script>
 
-<a href="/recipes/1">
-    <div class="bg-card rounded-lg shadow-sm overflow-hidden transition-all duration-300">
-            <img src={fruits} alt="fruits" class="w-full h-52 object-cover" />
-        <div class="p-4 space-y-2">
-            <h3 class="font-semibold text-base">
-                My super duper tasty recipe
-            </h3>
-            <div class="text-sm text-muted-foreground grid space-y-1">
-                <div class="flex justify-between">
-                    <span>Time</span>
-                    <span>45 min</span>
-                </div>
-                <Separator />
-                <div class="flex justify-between">
-                    <span>Servings</span>
-                    <Badge>4</Badge>
-                </div>
-                <Separator />
-                <div class="flex justify-between">
-                    <span>Ingredients</span>
-                    <Badge>12</Badge>
-                </div>
+<div class="bg-card rounded-lg shadow-sm overflow-hidden transition-all duration-300">
+    <div class="relative">
+        <Badge variant="secondary" class="absolute top-2 left-2">
+            <ClockIcon /> 45m
+        </Badge>
+        <img src={fruits} alt="fruits" class="w-full h-52 object-cover" />
+    </div>
+    <div class="pt-4 px-4 pb-2 space-y-2">
+        <h3 class="font-semibold text-base">
+            My super duper tasty recipe
+        </h3>
+        <div class="text-sm text-muted-foreground grid space-y-1">
+            <div class="flex justify-between">
+                <span>Servings</span>
+                <Badge>4</Badge>
             </div>
+            <Separator />
+            <div class="flex justify-between">
+                <span>Ingredients</span>
+                <Badge>12</Badge>
+            </div>
+            <Separator />
         </div>
     </div>
-</a>
+    <ScrollArea class="whitespace-nowrap pb-4 px-4" orientation="horizontal">
+        <div class="flex w-max gap-x-1">
+            <Badge variant="secondary" class="text-muted-foreground">Spicy</Badge>
+            <Badge variant="secondary" class="text-muted-foreground">Mediterranean</Badge>
+            <Badge variant="secondary" class="text-muted-foreground">Lunch</Badge>
+            <Badge variant="secondary" class="text-muted-foreground">Pasta</Badge>
+            <Badge variant="secondary" class="text-muted-foreground">Organic</Badge>
+            <Badge variant="secondary" class="text-muted-foreground">Healthy</Badge>
+            <Badge variant="secondary" class="text-muted-foreground">Quick</Badge>
+        </div>
+    </ScrollArea>
+</div>
