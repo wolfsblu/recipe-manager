bdcd59752197e1131b1c630807918afe000c07fb
Lukas Wolfsberger <1245519+wolfsblu@users.noreply.github.com>
2025-09-09 20:56:29 +0200

    Fix bug in transaction handling

--- infra/sqlite/transactions.go
+++ infra/sqlite/transactions.go
@@ -2,10 +2,14 @@ package sqlite
 
 import (
 	"context"
+
 	"github.com/wolfsblu/go-chef/domain"
 )
 
 func (s *Store) Begin(ctx context.Context) error {
+	if s.tx != nil {
+		return nil
+	}
 	tx, err := s.db.BeginTx(ctx, nil)
 	if err != nil {
 		return domain.WrapError(domain.ErrStartingTransaction, err)
@@ -16,6 +20,13 @@ func (s *Store) Begin(ctx context.Context) error {
 }
 
 func (s *Store) Commit() error {
+	if s.tx == nil {
+		return nil
+	}
+	defer func() {
+		s.qtx = nil
+		s.tx = nil
+	}()
 	err := s.tx.Commit()
 	if err != nil {
 		return domain.WrapError(domain.ErrCommittingTransaction, err)
@@ -24,6 +35,9 @@ func (s *Store) Commit() error {
 }
 
 func (s *Store) Rollback() {
+	if s.tx == nil {
+		return
+	}
 	defer func() {
 		s.qtx = nil
 		s.tx = nil
